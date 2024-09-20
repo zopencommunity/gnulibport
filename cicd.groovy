57456bd754f0e1a908fbf5f47fd858700e5e34f4
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:14 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/gnulibport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/gnulibport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gnulibport.git'), string(name: 'PORT_DESCRIPTION', value: 'Gnulib is a central location for common GNU code, intended to be shared among GNU packages.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/gnulibport.git'), string(name: 'PORT_DESCRIPTION', value: 'Gnulib is a central location for common GNU code, intended to be shared among GNU packages.' )]
   }
 }
